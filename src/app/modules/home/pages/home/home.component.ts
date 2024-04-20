05bacadf34118c43190c30cd4b2bc9863dbc6a8d
Nicolás Marulanda P <nmarulo@outlook.com>
2024-04-21 00:41:30 +0200

    Modificado: Página de inicio - Se agregó un "stretched-link" (de bootstrap) y se modificó un poco el diseño. - Ahora se usa signal para obtener la respuesta de la API.

--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -1,27 +1,41 @@
-import {Component, OnInit} from '@angular/core';
-import {FindAllShoppingListRes} from '../../../../models/find-all-shopping-list-res';
-import {Observable} from 'rxjs';
+import {Component, OnInit, signal} from '@angular/core';
+import {tap} from 'rxjs';
 import {ShoppingListsService} from '../../../../services/shopping-lists/shopping-lists.service';
 import {AsyncPipe} from '@angular/common';
+import {RouterLink} from '@angular/router';
+import {FindAllShoppingListRes} from '../../../../models/find-all-shopping-list-res';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    AsyncPipe
+    AsyncPipe,
+    RouterLink
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
 
-  shoppingListsRes$ = new Observable<FindAllShoppingListRes>();
+  private readonly _initShoppingList: FindAllShoppingListRes = {
+    content: [],
+    currentPage: 0,
+    pageSize: 0,
+    totalPages: 0,
+    total: 0
+  };
+
+  shoppingListsRes = signal<FindAllShoppingListRes>(this._initShoppingList);
 
   constructor(private shoppingListsService: ShoppingListsService) {
   }
 
   ngOnInit() {
-    this.shoppingListsRes$ = this.shoppingListsService.getShoppingLists();
+    this.shoppingListsService.getShoppingLists()
+        .pipe(
+          tap(shoppingLists => this.shoppingListsRes.set(shoppingLists))
+        )
+        .subscribe();
   }
 
 }
