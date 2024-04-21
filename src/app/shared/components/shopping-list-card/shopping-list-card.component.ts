c8b98e76d5430106102fc966b31a19405b3adfb6
Nicolás Marulanda P <nmarulo@outlook.com>
2024-04-21 23:27:50 +0200

    Nuevo componente ShoppingListCardComponent

---
+++ src/app/shared/components/shopping-list-card/shopping-list-card.component.ts
@@ -0,0 +1,18 @@
+import {Component, input} from '@angular/core';
+import {ShoppingList} from '../../../models/find-all-shopping-list-res';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-shopping-list-card',
+  standalone: true,
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './shopping-list-card.component.html',
+  styleUrl: './shopping-list-card.component.css'
+})
+export class ShoppingListCardComponent {
+
+  response = input.required<ShoppingList>();
+
+}
