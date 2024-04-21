fa83c5b59a26cfa2ee7cbfb0cc337bcb40c92178
Nicolás Marulanda P <nmarulo@outlook.com>
2024-04-21 23:26:50 +0200

    Actualizado componente app - Ahora usa los componentes "HeaderComponent", "NavbarComponent" y "SidebarComponent"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HeaderComponent} from './layout/header/header.component';
+import {NavbarComponent} from './layout/navbar/navbar.component';
+import {SidebarComponent} from './layout/sidebar/sidebar.component';
+import {DataSidebar} from './models/data-sidebar';
+import {SidebarService} from './services/sidebar.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    NavbarComponent,
+    SidebarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'despensa-web';
+
+  private readonly _sidebar: DataSidebar[] = [
+    {
+      path: '/shopping-list',
+      text: 'Lista de la compra'
+    }
+  ];
+
+  constructor(private sidebarService: SidebarService) {
+    this.sidebarService.setElements(this._sidebar);
+  }
+
 }
