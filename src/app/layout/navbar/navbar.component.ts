42eb433ca2107b8afb93f1397238428b5f51502b
Nicolás Marulanda P <nmarulo@outlook.com>
2024-04-27 14:08:21 +0200

    Modificado componente header y navbar - Se reemplaza la implementación, por el uso de proyecciones con "ng-content"

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,23 +1,14 @@
 import {Component} from '@angular/core';
-import {RouterLink} from '@angular/router';
-import {NavbarService} from '../../services/navbar.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [
-    RouterLink
-  ],
+  imports: [],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
-  constructor(private navbarService: NavbarService) {
+  constructor() {
   }
-
-  getElements() {
-    return this.navbarService.getElements();
-  }
-
 }
