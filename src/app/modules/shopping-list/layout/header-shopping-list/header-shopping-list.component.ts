1643487c33c5431d89c206efa356918b30c876af
Nicolás Marulanda P <nmarulo@outlook.com>
2024-04-27 18:14:37 +0200

    Página "ver lista" - Encabezado - Se mueva al componente header el elemento con el nombre de la lista y campo input de edición.

--- src/app/modules/shopping-list/layout/header-shopping-list/header-shopping-list.component.ts
+++ src/app/modules/shopping-list/layout/header-shopping-list/header-shopping-list.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {HeaderComponent} from '../../../../layout/header/header.component';
 import {NgClass} from '@angular/common';
@@ -15,5 +15,9 @@ import {NgClass} from '@angular/common';
   styleUrl: './header-shopping-list.component.css'
 })
 export class HeaderShoppingListComponent {
+
+  @Input() isEdit: boolean = false;
+
+  @Input() name!: string;
 
 }
