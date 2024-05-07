69ae9e6de97d90fae17fabc9bf6e5d8c3c281f82
Nicolás Marulanda P <nmarulo@outlook.com>
2024-05-07 21:48:14 +0200

    Página "ver lista" - Barra de navegación - Se reemplazan los botones por los componentes de primeng.

--- src/app/modules/shopping-list/layout/navbar-shopping-list/navbar-shopping-list.component.ts
+++ src/app/modules/shopping-list/layout/navbar-shopping-list/navbar-shopping-list.component.ts
@@ -1,13 +1,15 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {NavbarComponent} from '../../../../layout/navbar/navbar.component';
+import {ButtonModule} from 'primeng/button';
 
 @Component({
   selector: 'app-navbar-shopping-list',
   standalone: true,
   imports: [
     RouterLink,
-    NavbarComponent
+    NavbarComponent,
+    ButtonModule
   ],
   templateUrl: './navbar-shopping-list.component.html',
   styleUrl: './navbar-shopping-list.component.css'
