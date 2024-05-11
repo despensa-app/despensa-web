9527fd4ff19fa15be45c978cec33775a20d963b8
Nicolás Marulanda P <nmarulo@outlook.com>
2024-05-11 20:43:28 +0200

    Página "Editar lista - Agregar producto" - Se define el componente de la página. - Se define la ruta. - Se consulta la lista de productos.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from './modules/home/pages/home/home.component';
 import {ShoppingListComponent} from './modules/shopping-list/pages/shopping-list/shopping-list.component';
+import {
+  AddProductsShoppingListComponent
+} from './modules/shopping-list/pages/add-products-shopping-list/add-products-shopping-list.component';
 
 export const routes: Routes = [
-
+  {
+    path: 'shopping-list/:id/add-products',
+    component: AddProductsShoppingListComponent
+  },
   {
     path: 'shopping-list/:id',
     component: ShoppingListComponent
---
+++ src/app/modules/shopping-list/pages/add-products-shopping-list/add-products-shopping-list.component.ts
@@ -0,0 +1,61 @@
+import {Component, Input, OnInit, signal} from '@angular/core';
+import {ButtonModule} from 'primeng/button';
+import {CheckboxModule} from 'primeng/checkbox';
+import {DialogModule} from 'primeng/dialog';
+import {HeaderShoppingListComponent} from '../../layout/header-shopping-list/header-shopping-list.component';
+import {ImageModule} from 'primeng/image';
+import {NavbarShoppingListComponent} from '../../layout/navbar-shopping-list/navbar-shopping-list.component';
+import {PageComponent} from '../../../../layout/page/page.component';
+import {
+  AddProductsNavbarShoppingListComponent
+} from '../../layout/add-products-navbar-shopping-list/add-products-navbar-shopping-list.component';
+import {
+  AddProductHeaderShoppingListComponent
+} from '../../layout/add-product-header-shopping-list/add-product-header-shopping-list.component';
+import {ProductsService} from '../../../../services/products/products.service';
+import {tap} from 'rxjs';
+import {FindAllShoppingListProductsRes} from '../../../../models/find-all-shopping-list-products-res';
+
+@Component({
+  selector: 'app-add-products-shopping-list',
+  standalone: true,
+  imports: [
+    ButtonModule,
+    CheckboxModule,
+    DialogModule,
+    HeaderShoppingListComponent,
+    ImageModule,
+    NavbarShoppingListComponent,
+    PageComponent,
+    AddProductsNavbarShoppingListComponent,
+    AddProductHeaderShoppingListComponent
+  ],
+  templateUrl: './add-products-shopping-list.component.html',
+  styleUrl: './add-products-shopping-list.component.css'
+})
+export class AddProductsShoppingListComponent implements OnInit {
+
+  private _initShoppingListProductsRes: FindAllShoppingListProductsRes = {
+    content: [],
+    currentPage: 0,
+    pageSize: 0,
+    totalPages: 0,
+    total: 0
+  };
+
+  shoppingListProductsRes = signal<FindAllShoppingListProductsRes>(this._initShoppingListProductsRes);
+
+  @Input('id') idShoppingList: number = 0;
+
+  constructor(private productsService: ProductsService) {
+  }
+
+  ngOnInit(): void {
+    this.productsService.findAllShoppingList(this.idShoppingList)
+        .pipe(
+          tap(shoppingList => this.shoppingListProductsRes.set(shoppingList))
+        )
+        .subscribe();
+  }
+
+}
