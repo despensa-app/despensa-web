fa98f175a5df41fdae994fed15114dba788b457d
Nicolás Marulanda P <nmarulo@outlook.com>
2024-05-11 20:39:36 +0200

    Página "Editar lista - Agregar producto" - Encabezado

---
+++ src/app/modules/shopping-list/layout/add-product-header-shopping-list/add-product-header-shopping-list.component.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+import {HeaderComponent} from '../../../../layout/header/header.component';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-add-product-header-shopping-list',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    RouterLink
+  ],
+  templateUrl: './add-product-header-shopping-list.component.html',
+  styleUrl: './add-product-header-shopping-list.component.css'
+})
+export class AddProductHeaderShoppingListComponent {
+
+}
