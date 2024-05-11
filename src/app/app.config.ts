3dcf7dc1b612d19ab8e533f5cecf2c0f9a2e5b51
Nicolás Marulanda P <nmarulo@outlook.com>
2024-05-11 18:17:34 +0200

    Agregado "providerAnimations" - Algunos componentes de primeng necesitan este provider.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,12 @@ import {provideRouter, withComponentInputBinding} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideHttpClient} from '@angular/common/http';
+import {provideAnimations} from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient()
+    provideHttpClient(),
+    provideAnimations()
   ]
 };
