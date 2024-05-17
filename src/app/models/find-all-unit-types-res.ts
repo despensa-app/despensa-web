24fa8b557e044415c8e0bcc7fa79a201728a8944
Nicolás Marulanda P <nmarulo@outlook.com>
2024-05-17 22:00:50 +0200

    Nuevo "unit-types.service" - findAll(): Obtener la lista de tipos de unidades.

---
+++ src/app/models/find-all-unit-types-res.ts
@@ -0,0 +1,12 @@
+import {PagingAndSortingRes} from './paging-and-sorting-res';
+
+export interface FindAllUnitTypesRes extends PagingAndSortingRes<UnitType> {
+
+}
+
+export interface UnitType {
+  id: number;
+  name: string;
+  totalProducts: number;
+  createdAt: string;
+}
---
+++ src/app/services/unit-types/unit-types.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {FindAllUnitTypesRes} from '../../models/find-all-unit-types-res';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UnitTypesService {
+
+  private readonly _unitTypesURI = `${environment.baseUrl}/unit-types`;
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  findAll() {
+    return this.httpClient.get<FindAllUnitTypesRes>(this._unitTypesURI);
+  }
+
+}
