978ac29cb640c0dea39bd31a3f994da737b3a738
Nicolás Marulanda P <nmarulo@outlook.com>
2024-05-26 02:41:48 +0200

    Modificado: Variable de entorno - Agrega variable "production".

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  production: false,
   DESPENSA_REST_API_URL: 'http://localhost:8080/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  production: true,
   DESPENSA_REST_API_URL: ''
 };
