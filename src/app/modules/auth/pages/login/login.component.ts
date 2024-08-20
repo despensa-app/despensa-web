78df1871be9d6a6eaeec5a4113dc7536a7ee95fa
Nicolás Marulanda P <nmarulo@outlook.com>
2024-08-21 00:51:22 +0200

    Página login - Petición API - Implementación del servicio "AuthenticationService". - Se agrega interactividad al formulario y se crea el método "loginSubmit" quien será el que realiza la petición a la API.

--- src/app/modules/auth/pages/login/login.component.ts
+++ src/app/modules/auth/pages/login/login.component.ts
@@ -1,8 +1,12 @@
 import {Component} from '@angular/core';
 import {PageComponent} from '../../../../layout/page/page.component';
 import {NavbarComponent} from '../../../../layout/navbar/navbar.component';
-import {RouterLink} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 import {HeaderComponent} from '../../../../layout/header/header.component';
+import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
+import {LoginAuthenticationReq} from '../../../../models/login-authentication-req';
+import {AuthenticationService} from '../../../../services/pages/authentication.service';
+import {tap} from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -11,11 +15,42 @@ import {HeaderComponent} from '../../../../layout/header/header.component';
     PageComponent,
     NavbarComponent,
     RouterLink,
-    HeaderComponent
+    HeaderComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  loginForm = this.formBuilder.nonNullable.group({
+    username: [''],
+    password: ['']
+  });
+
+  constructor(
+    private authenticationService: AuthenticationService,
+    private formBuilder: FormBuilder,
+    private router: Router) {
+  }
+
+  loginSubmit() {
+    const request: LoginAuthenticationReq = {
+      username: this.loginForm.value.username!,
+      password: this.loginForm.value.password!
+    };
+
+    this.authenticationService.login(request)
+        .pipe(
+          tap(loginResponse => {
+            //TODO: guardar el token
+          })
+        )
+        .subscribe({
+          complete: () => {
+            this.router.navigate([''])
+                .then();
+          }
+        });
+  }
 }
