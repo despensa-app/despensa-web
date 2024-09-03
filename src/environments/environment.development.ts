ee4544b380744f99db93a523eaabd5948e9b242f
Nicolás Marulanda P <nmarulo@outlook.com>
2024-09-03 18:38:04 +0200

    Archivo "environment" - Algolia - Se actualiza las claves de Algolia a unas más restrictivas. Se limitan las peticiones por hora (100/h para el entorno de desarrollo).

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   DESPENSA_REST_API_URL: 'http://localhost:8080/api',
   APP_ID_ALGOLIA: 'FV53FOCWIA',
-  API_KEY_ALGOLIA: 'a7bda8435b0b8b315c2139285c5f74b1',
+  API_KEY_ALGOLIA: 'aa520441551ac813f4cedc93cfe0ac91',
   PRODUCTS_INDEX_NAME_ALGOLIA: 'dev_products'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   DESPENSA_REST_API_URL: 'https://despensa-rest-api.nmarulo.dev/api',
   APP_ID_ALGOLIA: 'FV53FOCWIA',
-  API_KEY_ALGOLIA: 'a7bda8435b0b8b315c2139285c5f74b1',
+  API_KEY_ALGOLIA: '739b43688e71ecd222005f9a079dddc8',
   PRODUCTS_INDEX_NAME_ALGOLIA: 'prod_products'
 };
