23e0c9da181764b339af275c959172ec71d023fd
Nicolás Marulanda P <nmarulo@outlook.com>
2024-09-28 13:25:19 +0200

    Nuevo diseño base - Página de listas de la compra - Actualizado diseño de los elementos de las listas (card). - Actualizado diseño del encabezado. - Actualizado diseño del menu de navegación del pie de página. Se establece las opciones de la ventana modal de acciones.

--- src/app/modules/home/layout/navbar-home/navbar-home.component.ts
+++ src/app/modules/home/layout/navbar-home/navbar-home.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {RouterLink} from '@angular/router';
 import {NavbarComponent} from '../../../../layout/navbar/navbar.component';
+import {ActionModal} from '../../../../shared/models/action-modal.model';
 
 @Component({
   selector: 'app-navbar-home',
@@ -13,5 +14,64 @@ import {NavbarComponent} from '../../../../layout/navbar/navbar.component';
   styleUrl: './navbar-home.component.css'
 })
 export class NavbarHomeComponent {
+
+  actionsModal: ActionModal[] = [
+    {
+      id: 'filter',
+      tab: {
+        icon: 'fas fa-filter',
+        title: 'Filtrar',
+        default: true
+      },
+      content: [
+        {
+          icon: {
+            left: 'fas fa-calendar-alt',
+            right: 'fas fa-angle-right'
+          },
+          title: 'Fecha',
+          disabled: true
+        },
+        {
+          icon: {
+            left: 'fas fa-star',
+            right: 'fas fa-angle-right'
+          },
+          title: 'Favoritos',
+          disabled: true
+        }
+      ]
+    },
+    {
+      id: 'sort',
+      tab: {
+        icon: 'fas fa-filter',
+        title: 'Ordenar'
+      },
+      content: [
+        {
+          icon: {
+            right: 'fas fa-long-arrow-alt-down'
+          },
+          title: 'Fecha de actualización',
+          disabled: true
+        },
+        {
+          icon: {
+            right: 'fas fa-long-arrow-alt-down'
+          },
+          title: 'Fecha de creación',
+          disabled: true
+        },
+        {
+          icon: {
+            right: 'fas fa-long-arrow-alt-down'
+          },
+          title: 'Título',
+          disabled: true
+        }
+      ]
+    }
+  ];
 
 }
