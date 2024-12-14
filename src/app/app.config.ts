4646b0470b4fe79dbea7c3281dc8e561041cb740
Nicolás Marulanda P <nmarulo@outlook.com>
2024-12-14 20:20:44 +0100

    Actualización: primeng 18 - Se define el layer para el css de primeng y para los CSS que tenía previamente, de esta forma le puedo definir la prioridad.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,9 @@ import {routes} from './app.routes';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {authInterceptor} from './shared/interceptor/auth.interceptor';
+import {providePrimeNG} from 'primeng/config';
+import Lara from '@primeng/themes/lara';
+import {definePreset} from '@primeng/themes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +17,34 @@ export const appConfig: ApplicationConfig = {
         authInterceptor
       ])
     ),
-    provideAnimations()
+    provideAnimations(),
+    providePrimeNG({
+      theme: {
+        preset: definePreset(Lara, {
+          semantic: {
+            primary: {
+              50: '{blue.50}',
+              100: '{blue.100}',
+              200: '{blue.200}',
+              300: '{blue.300}',
+              400: '{blue.400}',
+              500: '{blue.500}',
+              600: '{blue.600}',
+              700: '{blue.700}',
+              800: '{blue.800}',
+              900: '{blue.900}',
+              950: '{blue.950}'
+            }
+          }
+        }),
+        options: {
+          darkModeSelector: 'none',
+          cssLayer: {
+            name: 'primeng',
+            order: 'primeng, admin-lte, app-global-style'
+          }
+        }
+      }
+    })
   ]
 };
